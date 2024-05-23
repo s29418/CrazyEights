44cbed2480423f57be0874babf4541395a717d5b
Mikolaj Kulas <misionn5@gmail.com>
2024-05-23 11:09:02 +0200

    Added Deck class, and it's creation method

--- Deck.py
+++ Deck.py
@@ -1,4 +1,13 @@
+import random
+
+
 class Deck:
 
     def __init__(self):
-        #d
+        self.cards = self.create_deck()
+        random.shuffle(self.cards)
+
+    def create_deck(self):
+        suits = ['H', 'D', 'C', 'S']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        return [(rank, suit) for suit in suits for rank in ranks]
