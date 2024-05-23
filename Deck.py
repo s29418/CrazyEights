2fc85afee2f39cb16dfdb200b10aca8671b2372a
Mikolaj Kulas <misionn5@gmail.com>
2024-05-23 11:30:30 +0200

    Added draw_card and deal_cards methods.

--- Deck.py
+++ Deck.py
@@ -11,3 +11,15 @@ class Deck:
         suits = ['H', 'D', 'C', 'S']
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         return [(rank, suit) for suit in suits for rank in ranks]
+
+    def draw_card(self):
+        return self.cards.pop()
+
+    def deal_cards(self, number_of_players):
+        player_cards = [[] for _ in range(number_of_players)]
+
+        for c in range(8):
+            for player in player_cards:
+                player.append(self.draw_card())
+
+        return player_cards
