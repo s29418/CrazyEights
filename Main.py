4923184291c67e8c152e2aca57f934f2e7914784
Mikolaj Kulas <misionn5@gmail.com>
2024-05-30 14:15:30 +0200

    Final tweaks

--- Main.py
+++ Main.py
@@ -2,37 +2,36 @@ from CrazyEightsGame import CrazyEightsGame
 
 
 def show_instructions():
-    instructions = [
-        "\n=== Crazy Eights (Szalone Ósemki) ==="
-        "\nCrazy Eights to gra karciana, podobna do UNO, w której celem jest pozbycie się wszystkich swoich kart. "
-        
-        "\nGracz, który jako pierwszy pozbędzie się wszystkich swoich kart, wygrywa!",
+    instructions = (
+        "\n=== Crazy Eights (Szalone Ósemki) ===\n"
+        "Crazy Eights to gra karciana, podobna do UNO, w której celem jest pozbycie się wszystkich swoich kart. "
+        "Gracz, który jako pierwszy pozbędzie się wszystkich swoich kart, wygrywa!\n"
 
-        "\n=== Zasady gry: ==="
-        "\n1. == Rozdanie kart: =="
-        "\n   - Każdemu z graczy rozdaje się po 8 kart."
-        "\n   - Jedna karta zostaje odłożona na stos jako karta początkowa.",
+        "\n=== Zasady gry: ===\n"
+        "1. == Rozdanie kart: ==\n"
+        "   - Każdemu z graczy rozdaje się po 8 kart.\n"
+        "   - Jedna karta zostaje odłożona na stos jako karta początkowa.\n"
 
-        "\n2. == Przebieg gry: =="
-        "\n    - Gracz w swojej turze musi wyłożyć kartę, która zgadza się numerem lub kolorem z kartą na górze stosu. "
-        "Jeśli nie ma odpowiedniej karty, musi dobrać jedną kartę z talii."
-        "\n    - Wyłożona karta staje się nową kartą na górze stosu.",
+        "2. == Przebieg gry: ==\n"
+        "   - Gracz w swojej turze musi wyłożyć kartę, która zgadza się numerem lub kolorem z kartą na górze stosu.\n"
+        "     Jeśli nie ma odpowiedniej karty, musi dobrać jedną kartę z talii.\n"
+        "   - Wyłożona karta staje się nową kartą na górze stosu.\n"
 
-        "\n3. == Karty specjalne: =="
-        "\n    - Ósemki (8): Karta z numerem 8 dowolnego koloru jest kartą specjalną. Można ją położyć na dowolną inną "
-        "kartę. Po zagraniu ósemki, gracz wybiera nowy kolor, który musi być zagrany przez następnych graczy.",
+        "3. == Karty specjalne: ==\n"
+        "   - Ósemki (8): Karta z numerem 8 dowolnego koloru jest kartą specjalną. Można ją położyć na dowolną inną\n"
+        "     kartę. Po zagraniu ósemki, gracz wybiera nowy kolor, który musi być zagrany przez następnych graczy.\n"
 
-        "\n4. == Zakończenie gry: =="
-        "\n   - Gra kończy się, gdy jeden z graczy pozbędzie się wszystkich swoich kart. Ten gracz wygrywa.",
-    ]
+        "4. == Zakończenie gry: ==\n"
+        "   - Gra kończy się, gdy jeden z graczy pozbędzie się wszystkich swoich kart. Ten gracz wygrywa.\n"
 
-    i = 0
-    while i < len(instructions):
-        print(instructions[i])
-        user_input = input("\nNaciśnij 'd', aby przejść dalej, lub 'q' aby wyjść do menu: ")
-        if user_input.lower() == 'q':
-            return
-        i += 1
+        "\n== Ustawienia gry: ==\n"
+        "   - Gra pozwala na wybór ilości przeciwników: od jednego do trzech botów. Miłej zabawy!\n"
+    )
+
+    print(instructions)
+    user_input = input("\nNaciśnij 'q' i zatwierdź, aby wrócić do menu: ")
+    while user_input.lower() != 'q':
+        user_input = input("\nNiepoprawny klawisz. Naciśnij 'q', aby wrócić do menu: ")
 
 
 def main_menu():
@@ -45,11 +44,11 @@ def main_menu():
         choice = input("Wybierz opcję: ")
 
         if choice == '1':
-
             CrazyEightsGame()
         elif choice == '2':
             show_instructions()
         elif choice == '3':
+            print("Dziękujemy za grę! Do zobaczenia!")
             break
         else:
             print("Nieprawidłowa opcja. Spróbuj ponownie.")
